0c7b0b9798cf5a1f2a6dac461a7deeb7364b41e5
Yash Puthran <puthranyash1@gmail.com>
2018-03-01 14:10:58 +0530

    Added lottery enter form

--- src/App.js
+++ src/App.js
@@ -4,24 +4,64 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { manager: '' };
-  }
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: ''
+  };
 
   componentDidMount = async() => {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
   
+  onSubmit = async(event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    
+    this.setState({ message: 'Waiting for transaction to be verified...' });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'Successfully entered the lottery' });
+  }
+
   render() {
 
     return (
       <div>
         <h2>Lottery Contract</h2>
         <p>This contract is managed by {this.state.manager}</p>
+        <p>There are currently <strong>{this.state.players.length}</strong> people entered
+          competing to win <strong>{web3.utils.fromWei(this.state.balance, 'ether')}</strong> ether!
+        </p>
+        <hr/>
+
+        <form onSubmit= {this.onSubmit} action="">
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+            value = {this.state.value}
+            onChange = { event => this.setState({ value: event.target.value })}/>
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr/>
+
+        <div>
+          <h4>Transaction Status: {this.state.message}</h4>
+        </div>
       </div>
     );
   }
